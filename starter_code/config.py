322d9a1c28325c2957e94391a121d0bebf7a4b41
kenny95h <108397966+kenny95h@users.noreply.github.com>
2024-11-04 21:12:14 +0000

    First commit

---
+++ starter_code/config.py
@@ -0,0 +1,15 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# REMOVED PERSAONAL LOGIN DETAILS
+# Connect to the database
+db_name = ""
+db_pword = ""
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = f'postgresql://{db_name}:{db_pword}@localhost:5432/fyyurapp'
